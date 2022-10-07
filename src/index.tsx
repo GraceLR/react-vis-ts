6e5669349ab63338777837339ac0fb8d3d725060
GraceLR <gracerwork08@gmail.com>
2022-10-07 15:29:19 -0400

    added select and click events

--- src/index.tsx
+++ src/index.tsx
@@ -1,40 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Graph from "react-vis-ts";
-
-const graph = {
-  nodes: [
-    { id: 1, label: "Node 1", title: "node 1 tootip text" },
-    { id: 2, label: "Node 2", title: "node 2 tootip text" },
-    { id: 3, label: "Node 3", title: "node 3 tootip text" },
-    { id: 4, label: "Node 4", title: "node 4 tootip text" },
-    { id: 5, label: "Node 5", title: "node 5 tootip text" },
-  ],
-  edges: [
-    { from: 1, to: 2 },
-    { from: 1, to: 3 },
-    { from: 2, to: 4 },
-    { from: 2, to: 5 },
-  ],
-};
-const events = {
-  // select: function (event: any) {
-  //   // var { nodes, edges } = event;
-  // },
-};
-const options = {
-  layout: {
-    hierarchical: false,
-  },
-  nodes: {
-    widthConstraint: { minimum: 50 },
-  },
-  edges: {
-    color: "#000000",
-    length: "200",
-    smooth: { enabled: true, type: "dynamic" },
-  },
-};
+import DemoGraph from "./demoGraph";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -52,12 +18,7 @@ root.render(
         {` `}View on GitHub
       </a>
       <div>
-        <Graph
-          graph={graph}
-          options={options}
-          events={events}
-          style={{ height: "640px" }}
-        />
+        <DemoGraph />
       </div>
     </div>
   </React.StrictMode>
